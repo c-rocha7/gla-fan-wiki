75539f496727dc85dfacd6dea98aaef18e18a1aa
Cauã R. Pereira <7aauac@gmail.com>
2024-07-11 18:35:34 -0300

    feat: create relationship function to CharacterTag

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -6,6 +6,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Tag extends Model
 {
@@ -14,4 +15,9 @@ class Tag extends Model
     protected $guarded = [
         'id'
     ];
+
+    public function characterTag(): HasMany
+    {
+        return $this->hasMany(CharacterTag::class);
+    }
 }
