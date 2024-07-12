41a5c0b8490c9b5b84491cfd6a766b4d88eeb5e7
Cauã R. Pereira <7aauac@gmail.com>
2024-07-11 23:18:49 -0300

    feat: create model, factory, migration and seeder for Skill

---
+++ database/migrations/2024_07_11_231225_create_skills_table.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('skills', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('character_id')->constrained();
+            $table->string('image');
+            $table->string('name');
+            $table->tinyInteger('level');
+            $table->text('description');
+            $table->string('video');
+            $table->tinyInteger('recharge');
+            $table->integer('energy');
+            $table->float('pve_power');
+            $table->float('pvp_power');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('skills');
+    }
+};
