f7603848bb06fd9dc36a5f978dba63c70e8910a5
Cauã R. Pereira <7aauac@gmail.com>
2024-07-12 16:25:43 -0300

    feat: create model, factory, migration and seeder for ItemType

---
+++ database/seeders/ItemTypeSeeder.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ItemTypeSeeder extends Seeder
+{
+    public function run(): void
+    {
+    }
+}
