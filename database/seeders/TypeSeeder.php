4560655ec8bd102e4cc87a20a3357c9caddf7bce
Cauã R. Pereira <7aauac@gmail.com>
2024-07-12 12:15:01 -0300

    feat: create model, factory, migration and seeder for Type

---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TypeSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $data = [
+            [
+                'name' => 'Cabeça'
+            ],
+            [
+                'name' => 'Corpo'
+            ],
+            [
+                'name' => 'Pernas'
+            ],
+            [
+                'name' => 'Emblema'
+            ],
+            [
+                'name' => 'Arma'
+            ],
+            [
+                'name' => 'Acessório'
+            ]
+        ];
+
+        DB::table('types')->insert([
+            ...$data,
+        ]);
+    }
+}
