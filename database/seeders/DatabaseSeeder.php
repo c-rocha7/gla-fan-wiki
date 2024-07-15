6a4f54c24360d1137d63061677e795a3821a1849
Cauã R. Pereira <7aauac@gmail.com>
2024-07-15 11:18:46 -0300

    feat: add DropSeeder to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             BaseStatuSeeder::class,
             ItemSeeder::class,
             TypeSeeder::class,
+            DropSeeder::class,
         ]);
     }
 }
