2f2fb5cebbec608df5c9df648066f1d83ff6e68c
Cauã R. Pereira <7aauac@gmail.com>
2024-07-18 15:56:44 -0300

    feat: create model, factory, migration and seeder for Mob

---
+++ database/migrations/2024_07_18_145647_create_mobs_table.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('mobs', function (Blueprint $table) {
+            $table->id();
+            $table->string('image');
+            $table->string('name');
+            $table->tinyInteger('level');
+            $table->integer('life');
+            $table->integer('attack');
+            $table->integer('defense');
+            $table->integer('speed');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('mobs');
+    }
+};
