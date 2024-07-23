1d966a7ffce1d0bd1101e07c0c81eff974758456
Cauã R. Pereira <7aauac@gmail.com>
2024-07-23 11:45:43 -0300

    feat: create resource for tag

---
+++ app/Filament/Resources/Config/TagResource.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Filament\Resources\Config;
+
+use App\Filament\Resources\Config\TagResource\Pages;
+use App\Models\Tag;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class TagResource extends Resource
+{
+    protected static ?string $model                 = Tag::class;
+    protected static ?string $modelLabel            = 'Tag';
+    protected static ?string $pluralModelLabel      = 'Tags';
+    protected static ?string $recordTitleAttribute  = 'name';
+    protected static bool $shouldRegisterNavigation = false;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Grid::make()->schema([
+                    Forms\Components\Section::make()->schema([
+                        Forms\Components\TextInput::make('name')
+                            ->label('Nome')
+                            ->required(),
+                    ]),
+                ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->label('Nome'),
+            ])
+            ->filters([])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateHeading('Nenhuma Tag');
+    }
+
+    public static function getRelations(): array
+    {
+        return [];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index'  => Pages\ListTags::route('/'),
+            'create' => Pages\CreateTag::route('/create'),
+            'edit'   => Pages\EditTag::route('/{record}/edit'),
+        ];
+    }
+}
