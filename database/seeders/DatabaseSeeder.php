f33a282a4f7b884b3182b02f44deb1473a41f790
Cauã R. Pereira <7aauac@gmail.com>
2024-07-24 15:06:37 -0300

    feat: add CharacterBaseStatusSeeder in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             TagSeeder::class,
             CharacterTagSeeder::class,
             BaseStatusSeeder::class,
+            CharacterBaseStatusSeeder::class,
         ]);
     }
 }
