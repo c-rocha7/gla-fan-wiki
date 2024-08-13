d4c5666cbe8732eaca23e0e07a8ab6308802a949
Cauã R. Pereira <7aauac@gmail.com>
2024-08-13 13:52:05 -0300

    feat: create model, factory, migration and seeder EquipmentTag

---
+++ app/Models/EquipmentTag.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class EquipmentTag extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [
+        'id',
+    ];
+
+    public function equipment(): BelongsTo
+    {
+        return $this->belongsTo(Equipment::class);
+    }
+
+    public function tag(): BelongsTo
+    {
+        return $this->belongsTo(Tag::class);
+    }
+}
